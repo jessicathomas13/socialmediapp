398c601265c44a2d9ac33f82b2be7bb80d8a2a5b
jess79336 <127780039+jess79336@users.noreply.github.com>
2024-07-27 11:33:21 -0600

    changes made to topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,30 +1,45 @@
 import "./topbar.css";
-import {Search, Person} from "@material-ui/icons"
+import {Search, Person, Chat, Notifications} from "@material-ui/icons"
 
 
 export default function Topbar(){
     return(
-        <div className="topbarcont">
+        <div className="topbarContainer">
             <div className="topbarLeft">
                 <span className="badge">MySpace</span>
             </div>
             <div className="topbarCenter">
-            <Search />
-            <input placeholder = "Search" className="search"></input>
+                <div className="searchbar">
+                <Search className="searchIcon"/>
+                <input placeholder = "Search" className="searchInput"></input>
+                </div>
             </div>
             <div className="topbarRight">
-                <div className="links">Home</div>
-                <div className="links">Feed</div>
-            </div>
-            <div className="topbarIcon">
-                <div className="topbarItem">
-                    <Person />
-                    <span className="topbarIcons">1</span>
+                <div className="topbarlinks">
+                    <span className="links">Home</span>
+                    <span className="links">Feed</span>
                 </div>
-
-            </div>
             
+                <div className="topbarIcons">
+                    <div className="topbarItem">
+                        <Person />
+                        <span className="topbarIconbadge">1</span>
+                    </div>
+                    
+                    <div className="topbarItem">
+                        <Chat />
+                        <span className="topbarIconbadge">2</span>
+                    </div>
+
+                    <div className="topbarItem">
+                        <Notifications />
+                        <span className="topbarIconbadge">1</span>
+                    </div>
+
+                </div>
+            <img src="/assets/profiles/profile1.jpg" alt="" className="topbarImg" />
+            </div>
         </div>
-    )
+    );
         
 }
