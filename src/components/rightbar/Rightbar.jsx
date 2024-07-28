e0ea04f3e0a072bc23d83ed50d4e33d1e577a7dc
jeremy46081 <85791464+jeremy46081@users.noreply.github.com>
2024-07-28 09:57:20 -0600

    changes made

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -8,7 +8,6 @@ export default function Rightbar() {
           <img className="bdayImg" src="assets/bday.png" alt=""/>
           <span className="bdayTxt"> <b>John Paul</b> and 2 friends have their birthday today.</span>
         </div>
-        
         <h4 className="rbarTitle">Friends Online</h4>
         <ul className="FriendList">
           <li className="Friend">
@@ -23,19 +22,19 @@ export default function Rightbar() {
                 <img className="rightProfile" src="assets/profiles/profile4.jpg" alt=""/>
                 <span className="rightOnline"></span>
               </div>
-              <span className="Username">Jonathan David</span>
+              <span className="Username">Alissa May</span>
           </li><li className="Friend">
               <div className="rightImgContainer">
                 <img className="rightProfile" src="assets/profiles/profile5.jpg" alt=""/>
                 <span className="rightOnline"></span>
               </div>
-              <span className="Username">Kim Cho</span>
+              <span className="Username">Esther Chen</span>
           </li><li className="Friend">
               <div className="rightImgContainer">
                 <img className="rightProfile" src="assets/profiles/profile6.jpg" alt=""/>
                 <span className="rightOnline"></span>
               </div>
-              <span className="Username">Frank Baum</span>
+              <span className="Username">Mitchell Barnes</span>
           </li>
         </ul>
       </div>
