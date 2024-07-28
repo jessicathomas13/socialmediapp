0a3ffed5252d14cde8e26d114029407f7a2ed885
jess79336 <127780039+jess79336@users.noreply.github.com>
2024-07-28 10:36:51 -0600

    made user data

---
+++ src/userData.js
@@ -0,0 +1,135 @@
+export const Users = [
+    {
+        id:1,
+        profilePicture: "assets/profiles/profile1.jpg",
+        username: "Matthew Peterson",
+    },
+    {
+        id:2,
+        profilePicture: "assets/profiles/profile2.jpg",
+        username: "Katie Buchanan",
+    },
+    {
+        id:3,
+        profilePicture: "assets/profiles/profile3.jpg",
+        username: "John Reed",
+    },
+    {
+        id:4,
+        profilePicture: "assets/profiles/profile4.jpg",
+        username: "Alissa May",
+    },
+    {
+        id:5,
+        profilePicture: "assets/profiles/profile5.jpg",
+        username: "Esther Chen",
+    },
+    {
+        id:6,
+        profilePicture: "assets/profiles/profile.6jpg",
+        username: "Mitchell Barnes",
+    },
+    {
+        id:7,
+        profilePicture: "assets/profiles/profile7.jpg",
+        username: "Ethan Ramon",
+    },
+    {
+        id:8,
+        profilePicture: "assets/profiles/profile8.jpg",
+        username: "Michael Gonsalves",
+    },
+    {
+        id:9,
+        profilePicture: "assets/profiles/profile9.jpg",
+        username: "Andrea Picquadio",
+    },
+    {
+        id:10,
+        profilePicture: "assets/profiles/profile10.jpg",
+        username: "Emily Morinho",
+    },
+    {
+        id:11,
+        profilePicture: "assets/profiles/profile11.jpg",
+        username: "Brandon Davis",
+    },
+    {
+        id:12,
+        profilePicture: "assets/profiles/profile12.jpg",
+        username: "Daniel Lee",
+    },
+    {
+        id:13,
+        profilePicture: "assets/profiles/profile13.jpg",
+        username: "Julia Wilson",
+    },
+    {
+        id:14,
+        profilePicture: "assets/profiles/profile14.jpg",
+        username: "James King",
+    },
+    {
+        id:15,
+        profilePicture: "assets/profiles/profile15.jpg",
+        username: "Samuel Martinez",
+    },
+    {
+        id:16,
+        profilePicture: "assets/profiles/profile16.jpg",
+        username: "Camila Aguiar",
+    },
+    {
+        id:17,
+        profilePicture: "assets/profiles/profile17.jpg",
+        username: "Rachel Lewis",
+    },
+
+];
+
+export const Posts = [
+    {
+        id: 1,
+        description: "It's my first post!",
+        photo: "assets/posts/post1.jpg",
+        date:"20 seconds ago",
+        userID: 1,
+        like: 5,
+        comments: 1,
+    },
+    {
+        id: 2,
+        photo: "assets/posts/post2.jpg",
+        date:"10 minutes ago",
+        userID: 12,
+        like: 10,
+        comments: 3,
+    },
+    {
+        id: 3,
+        description: "carnival",
+        photo: "assets/posts/post3.jpg",
+        date:"20 minutes ago",
+        userID: 3,
+        like: 50,
+        comments: 5,
+    },
+    {
+        id: 4,
+        description: "Fun with friends!",
+        photo: "assets/posts/post4.jpg",
+        date:"30 minutes ago",
+        userID: 6,
+        like: 100,
+        comments: 4,
+    },
+    {
+        id: 5,
+        description: "Took a cooking class, we made lasagna :)",
+        photo: "assets/posts/post5.jpg",
+        date:"30 minutes ago",
+        userID: 10,
+        like: 45,
+        comments: 2,
+    },
+]
