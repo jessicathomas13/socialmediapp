6dc401784f89873f327d5bcbd0a884ecd27a1812
jess79336 <127780039+jess79336@users.noreply.github.com>
2024-07-28 21:10:25 -0600

    created the users and auth

---
+++ api/models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        min:3,
+        max:25,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        max:50,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        min:8,
+    },
+    profileImg:{
+        type:String,
+        default:"",
+    },
+    coverImg:{
+        type:String,
+        default:"",
+    },
+    followers:{
+        type:Array,
+        default:[],
+    },
+    following:{
+        type:Array,
+        default:[],
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false,
+    }
+},
+{timestamps:true}
+);
+
+module.exports = mongoose.model("User", UserSchema);
