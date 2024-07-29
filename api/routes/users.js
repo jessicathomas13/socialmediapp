addb2244934298684a085a50a68ac2187d12bd84
jess79336 <127780039+jess79336@users.noreply.github.com>
2024-07-29 13:56:20 -0600

    developed delete user and get user functions

--- api/routes/users.js
+++ api/routes/users.js
@@ -29,5 +29,41 @@ router.put("/:id", async(req, res)=>{
     }
 });
 
+// Delete user
+router.delete("/:id", async(req, res)=>{
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        
+        try{
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Account deleted");
+        }
+        catch(error){
+            return res.status(500).json(error);
+
+        }
+    
+    }
+    else{
+        return res.status(403).json("You can only delete your own account.");
+    }
+});
+
+// Get a user
+router.get("/:id", async(req, res)=>{
+    
+    try{
+        const user = await User.findById(req.params.id);
+        const {password,updatedAt, ...other} = user._doc
+        res.status(200).json(other);
+    }
+    catch(error){
+        return res.status(500).json(error);
+
+    }
+    
+    
+});
+
+
 
 module.exports = router
