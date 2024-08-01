3731b93aee9d212617e23e47e555a7e72af933a0
jess79336 <127780039+jess79336@users.noreply.github.com>
2024-07-31 20:45:17 -0600

    added context, reducer, actions and apiCall and updated the login and register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
+import { useContext } from "react";
 import Homepage from "./pages/home/Homepage";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
-
+import { Context } from "./context/Context";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
+  Link,
+  Navigate
 } from "react-router-dom";
 
 function App() {
+
+  const {user} = useContext(Context)
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Homepage/>}/>
+        <Route exact path="/" element={user ? <Homepage/> : <Login/>}/>
           
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/login" element={user ? <Navigate to="/"/> : <Login/>}/>
           
-        <Route path="/register" element={<Register/>}/>
+        <Route path="/register" element={user ? <Navigate to="/"/> : <Register/>}/>
           
         <Route path="/profile/:username" element={<Profile/>}/>
 
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,16 +1,31 @@
 import "./login.css"
-import {useRef} from "react";
+import {useContext, useRef} from "react";
+import {loginCall} from "../../apiCall"
+import {Context} from "../../context/Context"
+import {CircularProgress} from "@material-ui/core"
+import {useNavigate} from "react-router"
 
 export default function Login() {
   const email = useRef();
   const password = useRef();
+  const {user, isFetching,dispatch} = useContext(Context);
+  let navigate = useNavigate(); 
+  
+  const routeChange = () =>{ 
+    let path = `/register`; 
+    navigate(path);
+  }
+
+
   const handleClick = (e)=>{
     e.preventDefault()
-    console.log("clicked")
+    loginCall({email:email.current.value,password: password.current.value},dispatch)
   }
+
+
   return (
     <div className="login">
-      <div className="loginWrapper">
+      <div className="loginWrapper"> 
         <div className="loginLeft">
             <h3 className="loginLogo">MySpace</h3>
             <span className="loginDesc">
@@ -21,9 +36,13 @@ export default function Login() {
             <form className="loginBox" onSubmit={handleClick}>
                 <input placeholder="Email address" type="email" required className="loginInput" ref={email}/>
                 <input placeholder="Password" type="password" required minLength="8" className="loginInput" ref={password}/>
-                <button className="loginButton">Log In</button>
+                <button className="loginButton" type="submit" disabled={isFetching}>
+                  {isFetching ? <CircularProgress color="white" size="22px"/> : "Log In"}
+                </button>
                 <span className="loginForgot">Forgot Password</span>
-                <button className="loginRegisterButton">Create an account</button>
+
+                <button className="loginRegisterButton" onClick={routeChange}>{isFetching ? <CircularProgress color="white" size="22px"/> : "Create an account"}</button>
+                
             </form>
         </div>
       </div>
